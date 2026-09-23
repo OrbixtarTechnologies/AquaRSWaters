968cde61b36df251c632c93d5551b1536d3a3b21
Usman Liaqat <137144786+OrbixtarTechnologies@users.noreply.github.com>
2026-09-22 17:04:03 -0700

    Remove placeholder cart workflow from public company site

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { useEffect } from "react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import gsap from "gsap";
 gsap.registerPlugin(ScrollTrigger);
-import { CartProvider } from "./context/CartContext";
 import { useLenis } from "./hooks/useLenis";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -13,22 +12,23 @@ import Products from "./pages/Products";
 import Services from "./pages/Services";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Cart from "./pages/Cart";
 import Privacy from "./pages/Privacy";
 import Terms from "./pages/Terms";
 
 function ScrollToTop() {
   const { pathname } = useLocation();
+
   useEffect(() => {
     window.scrollTo(0, 0);
-    // let the new page paint before recalculating trigger positions
     requestAnimationFrame(() => ScrollTrigger.refresh());
   }, [pathname]);
+
   return null;
 }
 
 function AppShell() {
   useLenis();
+
   return (
     <div className="min-h-full flex flex-col">
       <ScrollToTop />
@@ -40,7 +40,6 @@ function AppShell() {
           <Route path="/services" element={<Services />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/cart" element={<Cart />} />
           <Route path="/privacy" element={<Privacy />} />
           <Route path="/terms" element={<Terms />} />
         </Routes>
@@ -54,9 +53,7 @@ function AppShell() {
 export default function App() {
   return (
     <BrowserRouter>
-      <CartProvider>
-        <AppShell />
-      </CartProvider>
+      <AppShell />
     </BrowserRouter>
   );
 }
