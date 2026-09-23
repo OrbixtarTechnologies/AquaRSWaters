570f3d0b596f201444bf58358f47993aed957b3c
Usman Liaqat <137144786+OrbixtarTechnologies@users.noreply.github.com>
2026-09-23 10:56:01 -0700

    Use type-only ReactNode import in policy layout

--- src/components/PolicyLayout.tsx
+++ src/components/PolicyLayout.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect } from "react";
+import { useEffect } from "react";\nimport type { ReactNode } from "react";
 import { Link } from "react-router-dom";
 
 type PolicySection = {
